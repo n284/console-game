ff189a74919d18dde2c2a0765edbc1b7d88b9b48
n284 <n_kyo56@icloud.com>
2024-01-05 23:01:14 +0900

    Card.java, MarkParams.java, NumberParams.java and CardFactory.java complete.

--- console_game/src/main/java/sevens/card/CardFactory.java
+++ console_game/src/main/java/sevens/card/CardFactory.java
@@ -1,5 +1,99 @@
 package sevens.card;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+import sevens.card.objects.Card;
+import sevens.card.utilities.params.MarkParams;
+import sevens.card.utilities.params.NumberParams;
+import utilities.MessageLoader;
+import utilities.exceptions.SystemException;
+
+/**
+ * トランプを管理するクラス
+ * カードクラスのインスタンス生成をして山を作成する
+ */
 public class CardFactory {
-    
+    /**
+     * 山を表すフィールド
+     */
+    private static List<Card> deck;
+
+    /**
+     * 山を初期化する
+     * 
+     * @param
+     * @throws SystemException 不明なエラーが発生した場合にスローする
+     * @return
+     */
+    private static void initialize() throws SystemException {
+        try {
+            List<MarkParams> markList = new ArrayList<>(Arrays.asList(MarkParams.values()));
+            List<NumberParams> numberList = new ArrayList<>(Arrays.asList(NumberParams.values()));
+            MarkParams JOKER_MARK = markList.get(0);
+            NumberParams JOKER_NUMBER = numberList.get(0);
+            markList.remove(0);
+            numberList.remove(0);
+            CardFactory.deck.clear();
+
+            for (MarkParams mark : markList) {
+                for (NumberParams number : numberList) {
+                    CardFactory.deck.add(new Card(number.getNumber(), mark.getMark()));
+                }
+            }
+
+            CardFactory.deck.add(new Card(JOKER_NUMBER.getNumber(), JOKER_MARK.getMark()));
+        } catch (NullPointerException | IndexOutOfBoundsException | UnsupportedOperationException | ClassCastException
+                | IllegalArgumentException e) {
+            throw new SystemException(500, MessageLoader.loadMessage("error.unknown"));
+        }
+    }
+
+    /**
+     * 山をシャフルする
+     * 
+     * @param
+     * @throws SystemException 不明なエラーが発生した場合にスローする
+     * @return
+     */
+    private static void suffle() throws SystemException {
+        try {
+            Collections.shuffle(CardFactory.deck);
+        } catch (UnsupportedOperationException e) {
+            throw new SystemException(500, MessageLoader.loadMessage("error.unknown"));
+        }
+
+    }
+
+    /**
+     * 山をシャフルする
+     * シード値を指定する場合に使用する
+     * 
+     * @param seed
+     * @throws SystemException 不明なエラーが発生した場合にスローする
+     * @return
+     */
+    private static void suffle(long seed) throws SystemException {
+        try {
+            Collections.shuffle(CardFactory.deck, new Random(seed));
+        } catch (UnsupportedOperationException e) {
+            throw new SystemException(500, MessageLoader.loadMessage("error.unknown"));
+        }
+    }
+
+    /**
+     * 山を取得する
+     * 
+     * @param
+     * @return {@link List <{@link Card}>
+     * @throws SystemException 不明なエラーが発生した場合にスローする
+     */
+    public static List<Card> getDeck() throws SystemException {
+        CardFactory.initialize();
+        CardFactory.suffle();
+        return CardFactory.deck;
+    }
 }
--- console_game/src/main/java/sevens/card/objects/Card.java
+++ console_game/src/main/java/sevens/card/objects/Card.java
@@ -1,5 +1,46 @@
 package sevens.card.objects;
 
+/**
+ * トランプのカードを表すクラス
+ */
 public class Card {
-    
+    /**
+     * カードの数字を表す
+     */
+    private int number;
+
+    /**
+     * カードの絵柄を表す
+     */
+    private String mark;
+
+    /**
+     * コンストラクタ
+     * 
+     * @param number
+     * @param mark
+     * 
+     */
+    public Card(int number, String mark) {
+        this.number = number;
+        this.mark = mark;
+    }
+
+    /**
+     * numberフィールドのゲッター
+     * 
+     * @return int
+     */
+    public int getNumber() {
+        return this.number;
+    }
+
+    /**
+     * markフィールドのゲッター
+     * 
+     * @return int
+     */
+    public String getMark() {
+        return this.mark;
+    }
 }
